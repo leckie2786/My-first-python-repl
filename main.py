faefb6b94a53608b35ee6f80801ca9b68836e903
LewisJenkins1 <>
2021-06-18 17:18:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("What is your name")
+name = input()
+print("Hello " + name +", how are you today(happy/sad)")
+feeling=input()
+if feeling=="happy":
+  print("That is very good then")
+elif feeling =="sad":
+  print("That's not good then")
+else:
+  print("OK")
