b1bae4ae5beea81604265e334ead7495e59c65ba
leckie2786 <80899768+leckie2786@users.noreply.github.com>
2021-08-22 19:54:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,7 @@ sport=input()
 if sport=="football" or sport=="football":
   print("What team do you support")
   team=input()
-  if team=="Scotland" or team=="scotland" :
-   print("You aren't very good are you?")
+  if team=="Italy" or team=="italy" :
+   print("Congrats then")
+  elif team=="England" or team=="england":
+    print("Close to victory, but not close enough")
